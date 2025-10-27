ed988b9a48866ab74d9b410593dfa5b1d5b62bde
Cory Hans <135175936+hansbuilt@users.noreply.github.com>
2025-10-26 20:47:59 -0500

    Added get all orders function

--- shopify_sim/shopify_helloworld.py
+++ shopify_sim/shopify_helloworld.py
@@ -33,3 +33,31 @@ def get_all_products_df():
     # missing pagination for now
 
     return df
+
+
+def get_all_orders_df():
+    """Return a df of all orders from the Shopify store."""
+
+    store_name = os.getenv("store_name")
+    access_token = os.getenv("access_token")
+
+    base_url = (
+        f"https://{store_name}.myshopify.com/admin/api/2025-10/orders.json"
+    )
+
+    headers = {
+        "X-Shopify-Access-Token": access_token,
+        "Content-Type": "application/json",
+    }
+
+    params = {"limit": 250}
+
+    response = requests.get(base_url, headers=headers, params=params)
+
+    data = response.json()
+
+    df = pd.json_normalize(data["orders"])
+
+    # missing pagination for now
+
+    return df
