e9580025e7423bfad45296526ffccdf36ccac0cd
Cory Hans <135175936+hansbuilt@users.noreply.github.com>
2026-01-04 17:39:43 -0600

    Corrected date used as order date

--- dbt_bigquery_project/models/marts/orders_daily_mart.sql
+++ dbt_bigquery_project/models/marts/orders_daily_mart.sql
@@ -1,7 +1,7 @@
 select
-    created_at as order_date,
+    date(timestamp(processed_at)) as order_date,
     count(distinct id) as order_count,
     sum(total_price) as revenue
 from {{ ref('orders_fact_staging') }}
-group by created_at
-order by created_at
+group by processed_at
+order by processed_at
